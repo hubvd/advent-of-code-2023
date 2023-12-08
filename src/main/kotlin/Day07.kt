24226468444fa6bd870bc0c6d25513ed30bbc3dc
Hubert Van De Walle <hubv@protonmail.com>
2023-12-08 13:40:21 +0100

    Clean day 07

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -12,14 +12,14 @@ fun main() {
         listOf(1, 1, 1, 1, 1),
     )
 
-    fun compareOccurrences(o1: List<String>, o2: List<String>): Int {
-        val groups1 = o1[0].groupBy { it }.mapValues { it.value.size }.values.sorted()
-        val groups2 = o2[0].groupBy { it }.mapValues { it.value.size }.values.sorted()
+    fun compareOccurrences(o1: String, o2: String): Int {
+        val groups1 = o1.groupBy { it }.mapValues { it.value.size }.values.sorted()
+        val groups2 = o2.groupBy { it }.mapValues { it.value.size }.values.sorted()
         return groups.indexOf(groups2).compareTo(groups.indexOf(groups1))
     }
 
-    fun compareCards(o1: List<String>, o2: List<String>): Int {
-        for ((c1, c2) in o1[0].toCharArray().zip(o2[0].toCharArray())) {
+    fun compareCards(o1: String, o2: String): Int {
+        for ((c1, c2) in o1.toCharArray().zip(o2.toCharArray())) {
             return when {
                 c1 == c2 -> continue
                 else -> cards.indexOf(c2).compareTo(cards.indexOf(c1))
@@ -28,44 +28,24 @@ fun main() {
         return 0
     }
 
-    fun expand(card: String): List<String> {
-        var values = listOf<String>()
-        for (c in card) {
-            values = when (c) {
-                'J' -> buildList {
-                    if (values.isEmpty()) {
-                        for (char in cards) {
-                            if (char == 'J') continue
-                            add(char.toString())
-                        }
-                    } else {
-                        values.forEach { current ->
-                            for (char in cards) {
-                                if (char == 'J') continue
-                                add(current + char)
-                            }
-                        }
-                    }
-                }
+    fun best(card: String): String {
+        val mostCommon = card.groupBy { it }
+            .mapValues { it.value.size }
+            .entries
+            .filter { it.key != 'J' }
+            .maxByOrNull { it.value }
+            ?.key
+            ?: 'A'
 
-                else -> {
-                    if (values.isEmpty()) listOf(c.toString())
-                    else values.map { it + c }
-                }
-            }
-        }
-        return values
+        return card.replace('J', mostCommon)
     }
-
-    fun best(card: String): String = expand(card)
-        .map { listOf(it) }
-        .sortedWith(::compareOccurrences)
-        .last()
-        .first()
 
     readLines()
         .map { it.split(' ') }
-        .sortedWith(Comparator(::compareOccurrences).then(::compareCards))
+        .sortedWith(
+            compareBy<List<String>, String>(::compareOccurrences) { it.first() }
+                .then(compareBy(::compareCards) { it.first() })
+        )
         .mapIndexed { index, (_, score) -> (index + 1) * score.toInt() }
         .sum()
         .let { println(it) }
@@ -73,17 +53,10 @@ fun main() {
     readLines()
         .asSequence()
         .map { it.split(' ') }
-        .sortedWith(Comparator<List<String>> { o1, o2 ->
-            compareOccurrences(
-                listOf(best(o1[0]), o1[1]),
-                listOf(best(o2[0]), o2[1])
-            )
-        }.then { a, b ->
-            compareCards(
-                listOf(a[0].replace('J', '1'), a[1]),
-                listOf(b[0].replace('J', '1'), b[1]),
-            )
-        })
+        .sortedWith(
+            compareBy<List<String>, String>(::compareOccurrences) { best(it.first()) }
+                .then(compareBy(::compareCards) { it.first().replace('J', '1') })
+        )
         .mapIndexed { index, (_, score) -> (index + 1) * score.toInt() }
         .sum()
         .let { println(it) }
