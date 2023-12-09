859faf9732f791627e0530d93160fb4d06f2cc96
Hubert Van De Walle <hubv@protonmail.com>
2023-12-09 12:10:00 +0100

    Clean day 09

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -3,31 +3,30 @@ package be.vandewalleh
 fun main() {
     val input = readLines().map { it.parseLongs() }
 
-    fun find(
-        line: List<Long>,
-        adder: MutableList<Long>.(Long) -> Unit,
-        getter: (List<Long>) -> Long,
-        operator: (Long, Long) -> Long,
-    ): Long {
+    fun find(line: List<Long>, position: List<*>.() -> Int, operator: (Long, Long) -> Long): Long {
         val lines = mutableListOf(ArrayDeque(line))
 
         while (!lines.last().all { it == 0L }) {
             lines += ArrayDeque(lines.last().zipWithNext().map { it.second - it.first })
         }
 
-        with(lines.last()) {
-            adder(0L)
-        }
+        lines.last().run { add(position(), 0L) }
 
         for (i in lines.size - 2 downTo 0) {
             with(lines[i]) {
-                adder(operator(lines[i].run(getter), lines[i + 1].run(getter)))
+                add(
+                    position(),
+                    operator(
+                        this[position()],
+                        lines[i + 1].run { this[position()] },
+                    ),
+                )
             }
         }
 
-        return lines.first().run(getter)
+        return lines.first().run { this[position()] }
     }
 
-    println(input.sumOf { find(it, { addLast(it) }, { it.last() }, { a, b -> a + b }) })
-    println(input.sumOf { find(it, { addFirst(it) }, { it.first() }, { a, b -> a - b }) })
+    println(input.sumOf { find(it, { lastIndex }, { a, b -> a + b }) })
+    println(input.sumOf { find(it, { 0 }, { a, b -> a - b }) })
 }
